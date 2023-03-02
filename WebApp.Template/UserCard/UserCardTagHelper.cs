c500f719777c519ce0cf4aadbba330a481b93727
Omer UNAL <omer.unal4@saglik.gov.tr>
2023-03-02 17:34:22 +0300

    Templatepattern uygulandı

---
+++ WebApp.Template/UserCard/UserCardTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Template.UserCard
+{
+    //<user-card app-user />
+    public class UserCardTagHelper : TagHelper
+    {
+        public AppUser AppUser { get; set; }
+
+        private readonly IHttpContextAccessor _httpContextAccesor;
+        public UserCardTagHelper(IHttpContextAccessor httpContextAccesor)
+        {
+            _httpContextAccesor = httpContextAccesor;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCardTemplate;
+            if(_httpContextAccesor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userCardTemplate = new PrimeUserCardTemplate();
+
+            }
+            else
+            {
+                userCardTemplate = new DefaultUserCardTemplate();
+            }
+            userCardTemplate.SetUser(AppUser);
+
+            output.Content.SetHtmlContent(userCardTemplate.Build()); // hangi sinıftan nesne ürettiysee 
+        }
+      
+    }
+}
